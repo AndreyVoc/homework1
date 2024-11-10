6d2c411beb1749af32b9b957e97f527cf87256fb
AndreyVoc <vosko2018@yahoo.com>
2024-11-10 21:07:32 +0300

    Add files via upload

---
+++ Homework 10.11.2024.py
@@ -0,0 +1,5 @@
+the_number_of_comleted_dz = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_task =Number_of_hours_spent / (float(the_number_of_comleted_dz))
+print('Курс:', Course_name, ', всего задач:', the_number_of_comleted_dz, ', затрачено часов:', Number_of_hours_spent, ', среднее время выполнения:', Time_for_one_task, 'часа.')
