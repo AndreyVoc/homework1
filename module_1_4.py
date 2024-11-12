ad3dbac71df839b3266f2f3294ce9fb9dca07526
AndreyVoc <vosko2018@yahoo.com>
2024-11-12 17:37:10 +0300

    modyle_1_4.ру (homework 12.11.2024)

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите произвольный текст: ')
+print('Количество символов в введенном  тексте: ', len(my_string))
+print('Строка в верхнем регистре:', my_string .upper())
+print('Строка в нижнем регистре:', my_string .lower())
+print('Строка в без пробелов:', my_string .replace(' ', '_'))
+print('Первый символ строки:', my_string[0])
+print('Последний символ строки: ', my_string[-1])
