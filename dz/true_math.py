9a1754ae5b218f5ce83d6c2883992a13bb9a6e50
AndreyVoc <vosko2018@yahoo.com>
2024-12-13 00:37:02 +0300

    Update true_math.py

--- dz/true_math.py
+++ dz/true_math.py
@@ -1,6 +1,7 @@
 from math import inf
 def divide(first,second):
     if second == 0:
-        print(inf)
+        return inf
     else:
-        print(first / second)
+        div_fs = first / second
+        return div_fs
