fe958cf7bf27f97fcfe1e4cb406ba4f871929613
AndreyVoc <vosko2018@yahoo.com>
2024-12-13 00:36:44 +0300

    Update homework09.12.24.py

--- dz/homework09.12.24.py
+++ dz/homework09.12.24.py
@@ -1,7 +1,11 @@
 from fake_math import divide as fd
 from true_math import divide as td
 
-fd(69, 3)
-fd(3, 0)
-td(49, 7)
-td(15, 0)
+rez1 = fd(69, 3)
+rez2 = fd(3, 0)
+rez3 = td(49, 7)
+rez4 = td(15, 0)
+print(rez1)
+print(rez2)
+print(rez3)
+print(rez4)
